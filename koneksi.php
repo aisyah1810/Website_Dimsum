1965951a9b30e8d790ac355ddc819d7aa418a400
aisyah1810 <aisyahsucimaisyaro@gmail.com>
2025-05-21 08:23:24 +0700

    db

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Ganti dengan informasi koneksi database kamu
-$host = 'localhost';    // Server database (biasanya localhost)
+$host = 'mysql.railway.internal';    // Server database (biasanya localhost)
 $username = 'root';     // Username database
-$password = '';         // Password database (kosong jika tidak ada)
-$database = 'dimsum_db'; // Nama database yang benar
+$password = 'SqrcXjjnHfwmpNNDgoxckGSvXtrHYRPv';         // Password database (kosong jika tidak ada)
+$database = 'railway'; // Nama database yang benar
 
 // Membuat koneksi ke database
 $koneksi = mysqli_connect($host, $username, $password, $database);
